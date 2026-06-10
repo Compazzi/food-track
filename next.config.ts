a0f2bd5e38b750ae6874df2ae8cb46f5fc0696f3
Compazzi <mcompazzi@gmail.com>
2026-06-09 22:55:48 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Mantenha qualquer outra configuração que já exista aqui
-  serverActions: {
-    allowedOrigins: [
-      "cmpzz-server.tail3be0dc.ts.net:3000", // O seu endereço seguro do Tailscale
-      "localhost:3000",
-      // Se você acessar pelo IP da rede local (ex: "192.168.1.50:3000"), adicione-o aqui também!
-    ],
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "cmpzz-server.tail3be0dc.ts.net:3000", // O seu endereço seguro do Tailscale
+        "localhost:3000",
+      ],
+    },
   },
 };
 
