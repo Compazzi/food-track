68ad21ce759f8797dcd623b23c0ab56db19fb0d3
Compazzi <mcompazzi@gmail.com>
2026-06-09 22:53:05 -0300

    Address bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // Mantenha qualquer outra configuração que já exista aqui
+  serverActions: {
+    allowedOrigins: [
+      "cmpzz-server.tail3be0dc.ts.net:3000", // O seu endereço seguro do Tailscale
+      "localhost:3000",
+      // Se você acessar pelo IP da rede local (ex: "192.168.1.50:3000"), adicione-o aqui também!
+    ],
+  },
 };
 
 export default nextConfig;
